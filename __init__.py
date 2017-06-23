ddecf895fe813fb4aed6ae1fcab9458b140bde82
awalende <awalende@techfak.uni-bielefeld.de>
2017-06-23 15:19:27 +0200

    raw database connection and user authemtification

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, flash, redirect, request, session, abort, url_for
 import os
+from db_connector import DB_Connector
 
 app = Flask(__name__)
+
+
+
+wrong_password = False
 
 @app.route('/')
 def homepage():
@@ -13,11 +18,21 @@ def homepage():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login_page():
-	if request.form['password'] == 'password' and request.form['username'] == 'admin':
-		session['username'] = 'admin'
-		session['logged_in'] = True
-	else:
-		flash('wrong password')
+	db = DB_Connector("localhost", "root", "master", "bibicreator")
+	try:
+		user = request.form['username']
+		pw = request.form['password']
+		data = db.queryAndResult("SELECT `Password` FROM `Users` WHERE `Name` = %s", user)
+		if data:
+			if data[0] == pw:
+				print("it worked")
+				session['username'] = user
+				session['logged_in'] = True
+		else:
+			flash("You have entered wrong credentials.")
+	except Exception as e:
+		print(e)
+		flash("You have entered wrong credentials.")
 	return homepage()
 
 
@@ -25,9 +40,11 @@ def login_page():
 def logout():
     session['logged_in'] = False
     session.pop(session['username'], None)
-    return redirect(url_for('homepage'))
+    return homepage()
+    #return redirect(url_for('homepage'))
 
 if __name__ == "__main__":
+
 	app.debug = True
 	app.secret_key = os.urandom(5000)
 	app.run()
---
+++ db_connector.py
@@ -0,0 +1,20 @@
+import pymysql
+
+'''
+Connects to the standard mysql database
+'''
+
+
+class DB_Connector:
+
+    db = None
+
+    def __init__(self, host, db_user, db_pass, database):
+        self.db = pymysql.connect(host, db_user, db_pass, database)
+
+    def queryAndResult(self, query, statement):
+        cursor = self.db.cursor()
+        cursor.execute(query, statement)
+        data = cursor.fetchone()
+        return data
+
